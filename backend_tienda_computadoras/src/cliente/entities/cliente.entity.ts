29b662f508a9aa58581b27dedadd5d480cc175e3
Jonadab Aarón Choque Garcia <baezc3284@gmail.com>
2026-04-27 15:41:23 -0400

    El cliente.entity

--- backend_tienda_computadoras/src/cliente/entities/cliente.entity.ts
+++ backend_tienda_computadoras/src/cliente/entities/cliente.entity.ts
@@ -1,1 +1,42 @@
-export class Cliente {}
+import { Usuario } from 'src/usuario/entities/usuario.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  OneToOne,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('cliente')
+export class Cliente {
+  @PrimaryGeneratedColumn('uuid')
+  id: string | undefined;
+
+  @Column('uuid')
+  usuario_id: string | undefined;
+
+  @Column('varchar', { length: 30 })
+  nombre_completo: string | undefined;
+
+  @Column('varchar', { length: 20 })
+  telefono: string | undefined;
+
+  @Column('varchar', { length: 50 })
+  nit_facturacion: string | undefined;
+
+  @CreateDateColumn({ name: 'fecha_creacion' })
+  fechaCreacion: Date | undefined;
+
+  @UpdateDateColumn({ name: 'fecha_modificacion' })
+  fechaModificacion: Date | undefined;
+
+  @DeleteDateColumn({ name: 'fecha_eliminacion' })
+  fechaEliminacion: Date | undefined;
+
+  @OneToOne(() => Usuario)
+  @JoinColumn({ name: 'usuario_id' })
+  usuario: Usuario | undefined;
+}
